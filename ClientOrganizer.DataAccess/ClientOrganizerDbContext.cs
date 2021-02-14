10313f46b18b9c0f276f943b1137424152f02641
NVV <uniondirfolder@gmail.com>
2021-02-14 13:48:51 -0800

    ***

---
+++ ClientOrganizer.DataAccess/ClientOrganizerDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+
+using ClientOrganizer.Model;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ClientOrganizer.DataAccess
+{
+    public class ClientOrganizerDbContext : DbContext
+    {
+        public ClientOrganizerDbContext():base("ClientOrganizerDb")
+        {
+
+        }
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
