a81c7b59d39a53e86ffb5035a23d7ce857a8313a
NVV <uniondirfolder@gmail.com>
2021-02-14 12:17:29 -0800

    hard part...

--- ClientOrganizer.UA/App.xaml.cs
+++ ClientOrganizer.UA/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
 
 namespace ClientOrganizer.UI
@@ -13,5 +8,12 @@ namespace ClientOrganizer.UI
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = new MainWindow(new ViewModel.MainViewModel(new Data.ClientDataService()));
+
+            mainWindow.Show();
+        }
+
     }
 }
